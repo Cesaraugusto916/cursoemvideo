1b62b9753bdc55fda3504d39e9f7e07679917b6f
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-06-17 14:30:14 -0300

    Refatora o programa clássico que exibe a mensagem "Olá, Mundo!". As melhorias incluem: - Adição de cabeçalho e rodapé para melhor organização e apresentação do programa. - Inclusão de uma pequena pausa ao final para facilitar a leitura.

--- python3-mundo01-exs/ex001.py
+++ python3-mundo01-exs/ex001.py
@@ -1,3 +1,16 @@
-msg = 'Olá, mundo! Tudo bem com vcs?'
+import time
 
-print(msg)
+# --- Corpo Principal do Programa ---
+print('--- MEU PRIMEIRO PROGRAMA PYTHON ---')
+print('Bem-vindo(a)! Este é o clássico "Olá, Mundo!".')
+print('=' * 50)
+
+# Definindo a mensagem
+mensagem_saudacao = 'Olá, Mundo! Tudo bem com vocês?'
+
+# Exibindo a mensagem
+print(mensagem_saudacao)
+
+print('\n' + '=' * 50)
+print('--- FIM DO PROGRAMA ---')
+time.sleep(1) # Pequena pausa para a mensagem ser lida
